03b1c7e371697130c5fb91e16be7dcca7c81d64d
Mika Busante <mika@mikabusante.com>
2019-07-30 12:10:16 -0400

    Add heroku env

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
   googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  cookieKey: process.env.COOKIE_KEY,
+  mongoURI: process.env.MONGO_URI
 };
