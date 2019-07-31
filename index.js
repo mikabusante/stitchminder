dcd08f76dd430236a42f475619763e03dc47a48d
Mika Busante <mika@mikabusante.com>
2019-07-31 09:40:09 -0400

    Tweak routes for production

--- index.js
+++ index.js
@@ -26,4 +26,13 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/threadRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(process.env.PORT || 5000);
